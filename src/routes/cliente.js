1ce55b3aaceac5405c28b71a3203e93ebeefc59e
jsophiazzy <j.sophia@aluno.ifsp.edu.br>
2026-09-03 16:03:50 -0300

    Atualização do CRUD do funeral (busca de serviços de um funeral)

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -69,58 +69,6 @@ router.get("/funerais/:cpf", async (req, res, next) => {
     }
 });
 
-// Operação Exclusiva - Buscar os serviços de um funeral
-router.get("/servicos/:id", async (req, res, next) => {
-    try {
-        const id = Number(req.params.id);
-
-        if (!Number.isInteger(id) || id <= 0) {
-            throw new Error("Informe um ID de funeral válido!");
-        }
-
-        const funeral = await db.query(
-            "SELECT * FROM funeral WHERE id = $1",
-            [id]
-        );
-
-        if (!funeral.rowCount) {
-            throw new Error("Funeral não encontrado!");
-        }
-
-        const servicos = await db.query(
-            `SELECT
-                s.id,
-                s.nome,
-                s.descricao,
-                s.valor
-
-            FROM funeral f
-
-            JOIN servico_funeral sf
-                ON f.id = sf.id_funeral
-
-            JOIN servico s
-                ON sf.id_servico = s.id
-
-            WHERE f.id = $1`,
-            [id]
-        );
-
-        if (!servicos.rowCount) {
-            throw new Error("Este funeral não possui serviços associados!");
-        }
-
-        return res.status(200).json({
-            msg: "Serviços encontrados com sucesso!",
-            funeral: funeral.rows[0].nome_falecido,
-            servicos: servicos.rows
-        });
-
-    } catch (error) {
-        return res.status(400).json({ msg: error.message });
-    }
-});
-
 // GET pelo CPF
 router.get("/:cpf", async (req, res, next) => {
     try {
